3f00f290515f5e9e8c607df8788f87b227db8bd1
aillieo <aillieo@163.com>
2023-04-22 20:44:53 +0800

    add documentation

--- EasyTimeSlicing/Scripts/Runtime/TimeSlicingUtils.cs
+++ EasyTimeSlicing/Scripts/Runtime/TimeSlicingUtils.cs
@@ -10,11 +10,17 @@ namespace AillieoUtils.EasyTimeSlicing
     using UnityEngine;
     using UnityEngine.Assertions;
 
+    /// <summary>
+    /// Provides utility methods for time-sliced execution of tasks.
+    /// </summary>
     public static class TimeSlicingUtils
     {
         private static int cachedFrameRate = int.MinValue;
         private static float cachedFrameInterval;
 
+        /// <summary>
+        /// Gets the time interval between frames based on <see cref="Application.targetFrameRate"/>.
+        /// </summary>
         public static float frameInterval
         {
             get
@@ -29,11 +35,17 @@ namespace AillieoUtils.EasyTimeSlicing
             }
         }
 
+        /// <summary>
+        /// Gets time elapsed since the start of the current frame.
+        /// </summary>
         public static float timeSinceFrameStart
         {
             get { return Time.realtimeSinceStartup - Time.unscaledTime; }
         }
 
+        /// <summary>
+        /// Gets the estimated remaining time available in the current frame for executing a task.
+        /// </summary>
         public static float timeBudgetEstimated
         {
             get
@@ -42,6 +54,12 @@ namespace AillieoUtils.EasyTimeSlicing
             }
         }
 
+        /// <summary>
+        /// Try to execute an <see cref="Action"/> within the given expected execution time.
+        /// </summary>
+        /// <param name="action">The <see cref="Action"/> to execute.</param>
+        /// <param name="expectedExecutionTime">The expected execution time in seconds.</param>
+        /// <returns>Whether the execution was executed.</returns>
         public static bool TryExecute(Action action, float expectedExecutionTime)
         {
             Assert.IsNotNull(action);
@@ -55,6 +73,14 @@ namespace AillieoUtils.EasyTimeSlicing
             return false;
         }
 
+        /// <summary>
+        /// Try to execute an <see cref="Action{T}"/> within the given expected execution time.
+        /// </summary>
+        /// <typeparam name="T">The type of data to pass to the <see cref="Action{T}"/>.</typeparam>
+        /// <param name="action">The <see cref="Action{T}"/> to execute.</param>
+        /// <param name="data">The data to pass to the <see cref="Action{T}"/>.</param>
+        /// <param name="expectedExecutionTime">The expected execution time in seconds.</param>
+        /// <returns>Whether the execution was executed.</returns>
         public static bool TryExecute<T>(Action<T> action, T data, float expectedExecutionTime)
         {
             Assert.IsNotNull(action);
@@ -68,7 +94,15 @@ namespace AillieoUtils.EasyTimeSlicing
             return false;
         }
 
-        public static bool TryExecute<T>(Func<T> func, float expectedExecutionTime, out T result)
+        /// <summary>
+        /// Try to execute a <see cref="Func{TResult}"/> within the given expected execution time.
+        /// </summary>
+        /// <typeparam name="TResult">The return type of the <see cref="Func{TResult}"/>.</typeparam>
+        /// <param name="func">The <see cref="Func{TResult}"/> to execute.</param>
+        /// <param name="expectedExecutionTime">The expected execution time in seconds.</param>
+        /// <param name="result">The result of the <see cref="Func{TResult}"/>.</param>
+        /// <returns>Whether the execution was executed.</returns>
+        public static bool TryExecute<TResult>(Func<TResult> func, float expectedExecutionTime, out TResult result)
         {
             Assert.IsNotNull(func);
 
@@ -82,6 +116,16 @@ namespace AillieoUtils.EasyTimeSlicing
             return false;
         }
 
+        /// <summary>
+        /// Try to execute a <see cref="Func{T, TResult}"/> within the given expected execution time.
+        /// </summary>
+        /// <typeparam name="T">The type of data to pass to the <see cref="Func{T, TResult}"/>.</typeparam>
+        /// <typeparam name="TResult">The return type of the <see cref="Func{TResult}"/>.</typeparam>
+        /// <param name="func">The <see cref="Func{T, TResult}"/> to execute.</param>
+        /// <param name="data">The data to pass to the <see cref="Func{T, TResult}"/>.</param>
+        /// <param name="expectedExecutionTime">The expected execution time in seconds.</param>
+        /// <param name="result">The result of the <see cref="Func{TResult}"/>.</param>
+        /// <returns>Whether the execution was executed.</returns>
         public static bool TryExecute<T, TResult>(Func<T, TResult> func, T data, float expectedExecutionTime, out TResult result)
         {
             Assert.IsNotNull(func);
